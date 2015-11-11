bcf220c1d10fd00517714e0f7c7543f2ec58b6ca
michel-cf <michel@creatingfuture.eu>
2015-11-11 14:14:29 +0100

    Cleanup code

--- src/main/java/com/parallax/server/common/cloudsession/rest/RestUserService.java
+++ src/main/java/com/parallax/server/common/cloudsession/rest/RestUserService.java
@@ -91,8 +91,6 @@ public class RestUserService {
             return validation.getValidationResponse();
         }
 
-        System.out.println("VALIDATED OK");
-
         try {
             UserRecord user = userService.register(server, email, password, passwordConfirm, locale, screenname);
             JsonObject json = new JsonObject();
